073ef8c9f9a5108e10422e3302787cd6c5cfeae2
suprabha14 <suprabhasahoo2002@gmail.com>
2024-11-09 01:35:10 +0530

    redirect url

--- frontend/src/components/EditiorNavbar.jsx
+++ frontend/src/components/EditiorNavbar.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import logo from "../images/Header_logo.jpg"
+import React from "react";
+import logo from "../images/Header_logo.jpg";
 import { FiDownload } from "react-icons/fi";
-
+import { Link } from "react-router-dom";
 
 const EditiorNavbar = () => {
   return (
     <>
       <div className="EditiorNavbar flex items-center justify-between px-[100px] h-[80px] bg-[#141414]">
         <div className="logo">
-          <img className='w-[150px] cursor-pointer' src={logo} alt="" />
+          <Link to="/">
+            <img className="w-[150px] cursor-pointer" src={logo} alt="" />
+          </Link>
         </div>
-        <p>File / <span className='text-[gray]'>My first project</span></p>
-        <i className='p-[8px] btn bg-black rounded-[5px] cursor-pointer text-[20px]'><FiDownload /></i>
+        <p>
+          File / <span className="text-[gray]">My first project</span>
+        </p>
+        <i className="p-[8px] btn bg-black rounded-[5px] cursor-pointer text-[20px]">
+          <FiDownload />
+        </i>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default EditiorNavbar
+export default EditiorNavbar;
